d3a1dfb39ff66f84083e8347089ca92d1f671daa
MichaelPeterson3D <138528391+MichaelPeterson3D@users.noreply.github.com>
2023-07-13 18:16:19 -0400

    UI stuff

--- Assets/Scripts/Managers/HUDManager.cs
+++ Assets/Scripts/Managers/HUDManager.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class HUDManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Health TextMeshPro 
+    [SerializeField]
+    TMP_Text PlayerHealthText;
+    TMP_Text PlayerAmmoInClipAndClipSize; //ammo in clip + clip size
+    TMP_Text PlayerReserveAmmo;
+
+    //slider
+    [SerializeField]
+    private Slider HealthSlider;
+
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
+
         
+        if(GameManager.Instance != null && GameManager.Instance.currentPlayer != null && GameManager.Instance.currentPlayer.GetComponentInChildren<Health>() != null)
+        {
+
+            HealthSlider.value = GameManager.Instance.currentPlayer.GetComponentInChildren<Health>().CurrentHealth;
+            Debug.Log(HealthSlider.value);
+            PlayerHealthText.text = GameManager.Instance.currentPlayer.GetComponentInChildren<Health>().CurrentHealth.ToString();
+
+        }
     }
 }
